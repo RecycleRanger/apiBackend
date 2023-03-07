702d51f0afc1855401016018c2eec8dd00275cd8
TasosFrago <tasosfragkopoulos03@gmail.com>
2023-03-07 21:54:18 +0200

    Prisma schema setup. Logo added

--- recycle-ranger-web/pages/_app.tsx
+++ recycle-ranger-web/pages/_app.tsx
@@ -1,8 +1,11 @@
+import { SessionProvider } from 'next-auth/react';
 import { AppProps } from "next/app";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps: { session, ...pageProps} }: AppProps) => {
   return (
-    <Component {...pageProps} />
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   );
 };
 
