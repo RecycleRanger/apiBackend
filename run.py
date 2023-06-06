de0242e9bf167b479cf959f0753762eeb37aad92
TasosFrago <tasosfragkopoulos03@gmail.com>
2023-06-06 12:32:26 +0300

    Minor Change

--- run.py
+++ run.py
@@ -2,14 +2,30 @@ import uvicorn
 import sys
 
 
+APP_IMPORT_STRING="app.main:app"
+HOST="0.0.0.0"
+PORT=5000
+
 def main():
     if len(sys.argv) > 1:
         if sys.argv[1] == "dev":
             import os
             cwd = os.path.join(os.getcwd(), "app")
-            uvicorn.run("app.main:app", host="127.0.0.1", port=5000, log_level="debug", reload=True, reload_dirs=[cwd])
+            uvicorn.run(
+                APP_IMPORT_STRING,
+                host=HOST,
+                port=PORT,
+                log_level="debug",
+                reload=True,
+                reload_dirs=[cwd]
+            )
         elif sys.argv[1] == "prod":
-            uvicorn.run("app.main:app", host="127.0.0.1", port=5000, log_level="info")
+            uvicorn.run(
+                APP_IMPORT_STRING,
+                host=HOST,
+                port=PORT,
+                log_level="info"
+            )
     else:
         print("Needs args to run")
 
